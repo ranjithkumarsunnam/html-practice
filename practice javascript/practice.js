385e899bbfefa955c1b9ffd82e05ebdb5c133626
ranjithkumarsunnam <sunnamranjith7@gmail>
2026-02-11 22:05:57 +0530

    practice of javaScript

---
+++ practice javascript/practice.js
@@ -0,0 +1,13 @@
+let students = ["john","joe","jonny","joy","adhi","raki"];
+console.log(students);
+console.log(students[0]);
+console.log(students[1]);
+console.log(students[2]);
+console.log(students[3]);
+console.log(students[4]);
+console.log(students[5]);
+
+let a = 25;
+let b = 25;
+let c = a+b;
+console.log(c);
